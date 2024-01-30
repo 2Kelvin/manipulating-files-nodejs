fbb624ff35e51ed399a645cfaf4ce45c105d57c4
2Kelvin <rocketmancodes02@gmail.com>
2024-01-30 11:42:28 +0300

    learnt to create, read, write and delete folders using nodejs

--- folders.js
+++ folders.js
@@ -2,27 +2,28 @@
 
 const fs = require('node:fs');
 
-// const projectFolder = 'testFolder';
+const projectFolder = 'testFolder';
 
 
-// // check if the folder exists
-// // fs.existsSync => returns True if file exists, false if otherwise
-// console.log(fs.existsSync(projectFolder));
+// check if the folder exists
+// fs.existsSync => returns True if file exists, false if otherwise
+console.log(fs.existsSync(projectFolder));
 
 
-// // creating a new folder
-// fs.mkdirSync(projectFolder);
+// creating a new folder
+fs.mkdirSync(projectFolder);
 
 
-// // reading the contents of a file
-// console.log(fs.readdirSync(projectFolder));
+// reading the contents of a file
+console.log(fs.readdirSync(projectFolder));
 
 
-// // renaming a folder
-// fs.rename(projectFolder, 'renamedFolder', (err) => {
-//     if (err) console.error(err);
-//     else console.log('Folder renamed successfully');
-// });
+// renaming a folder
+fs.rename(projectFolder, 'renamedFolder', (err) => {
+    if (err) console.error(err);
+    else console.log('Folder renamed successfully');
+});
+
 
 // removing an empty folder
 fs.rmdir('myProject/newFolder', (err) => {
